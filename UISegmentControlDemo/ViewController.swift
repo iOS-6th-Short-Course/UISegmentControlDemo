3eb2ed8e1b20951fb95a99ac47348a031164e3a7
Chhaileng Peng <chhailengpeng@gmail.com>
2018-12-09 12:58:26 +0700

    UISegmentControll

--- UISegmentControlDemo/ViewController.swift
+++ UISegmentControlDemo/ViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segment: UISegmentedControl!
+    
+    @IBAction func segmentChanged(_ sender: UISegmentedControl) {
+        if segment.selectedSegmentIndex == 0 {
+            view.backgroundColor = .white
+        } else if segment.selectedSegmentIndex == 1 {
+            view.backgroundColor = .blue
+        } else if segment.selectedSegmentIndex == 2 {
+            view.backgroundColor = .yellow
+        } else if segment.selectedSegmentIndex == 3 {
+            view.backgroundColor = .green
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        segment.insertSegment(withTitle: "Hello", at: 2, animated: true)
+        segment.removeSegment(at: 4, animated: true)
+        segment.selectedSegmentIndex = 2
+        
+        segment.setImage(#imageLiteral(resourceName: "home"), forSegmentAt: 0)
+        segment.setTitle("Blue", forSegmentAt: 1)
+        segment.setTitle("Yellow", forSegmentAt: 2)
+        segment.setTitle("Green", forSegmentAt: 3)
     }
 
 
